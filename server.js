515998d41aac6435238bb025898082276418ba94
Marcos Leite <m.abner@live.co.uk>
2023-06-21 23:34:52 -0300

    routes boilerplate

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 require('./config/database');
+
 const express = require('express');
 const app = express();
-
 const port = process.env.SERVER_PORT;
+
+app.use(express.json());
+
+const articlesRouter = require('./api/routes/articles');
+app.use('/articles', articlesRouter);
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
