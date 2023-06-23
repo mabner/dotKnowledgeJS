94db3cfa78957a98525ea40d9cfd9a61975a04bf
Marcos Leite <m.abner@live.co.uk>
2023-06-23 02:31:59 -0300

    adds swagger to api and redirects / to /api-docs

---
+++ api/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+	try {
+		res.redirect('/api-docs');
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+module.exports = router;
